59b62e4527d3cbdb599004befcf983911c6162c7
VapeStation04 <sistemas_02@vapestation.pe>
2026-05-28 15:41:36 -0500

    mozooo

--- static/src/app/screens/payment_screen.js
+++ static/src/app/screens/payment_screen.js
@@ -1,16 +1,17 @@
 /** @odoo-module **/
 import { PaymentScreen } from "@point_of_sale/app/screens/payment_screen/payment_screen";
 import { patch } from "@web/core/utils/patch";
-import { onWillStart, useState } from "@odoo/owl"; 
+import { onWillStart, useState } from "@odoo/owl";
 import { CustomGridPopup } from "@vape_pos_order_fields/app/components/custom_grid_popup";
 
 patch(PaymentScreen.prototype, {
-    
     setup() {
         super.setup(...arguments);
-        
-        this.vapeState = useState({ renderTrigger: 0 });
-        
+        this.vapeUiState = useState({
+            channelId: this.currentOrder?.vs_channel_id || null,
+            shippingTypeId: this.currentOrder?.vs_shipping_type_id || null,
+        });
+
         onWillStart(async () => {
             if (!this.pos.vape_channels) {
                 const domain = [["pos_config_ids", "in", [this.pos.config.id]]];
@@ -23,18 +24,34 @@ patch(PaymentScreen.prototype, {
             if (!this.pos.vape_shipping_types) {
                 const domain = [["pos_config_ids", "in", [this.pos.config.id]]];
                 this.pos.vape_shipping_types = await this.env.services.orm.searchRead(
-                    "vape.shipping.type", 
-                    domain, 
+                    "vape.shipping.type",
+                    domain,
                     ["name"]
                 );
             }
         });
     },
 
+    get selectedChannel() {
+        if (!this.vapeUiState.channelId) return null;
+        return this.pos.vape_channels?.find(
+            c => c.id === this.vapeUiState.channelId
+        ) || null;
+    },
+
+    get selectedShippingType() {
+        if (!this.vapeUiState.shippingTypeId) return null;
+        return this.pos.vape_shipping_types?.find(
+            s => s.id === this.vapeUiState.shippingTypeId
+        ) || null;
+    },
+
     openCustomPaymentPopup() {
         this.dialog.add(CustomGridPopup, {
             title: "Método de pago",
-            list: this.payment_methods_from_config.map(m => ({ id: m.id, label: m.name, item: m })),
+            list: this.payment_methods_from_config.map(m => ({
+                id: m.id, label: m.name, item: m
+            })),
             getPayload: (selected) => {
                 if (selected) this.addNewPaymentLine(selected);
             },
@@ -44,13 +61,14 @@ patch(PaymentScreen.prototype, {
     async openChannelPopup() {
         this.dialog.add(CustomGridPopup, {
             title: "Seleccionar Canal de Venta",
-            list: this.pos.vape_channels.map(c => ({ id: c.id, label: c.name, item: c, isChannel: true })),
+            list: this.pos.vape_channels.map(c => ({
+                id: c.id, label: c.name, item: c, isChannel: true
+            })),
             getPayload: (selected) => {
                 if (selected) {
-                    const channelId = selected.id || selected.item?.id || selected;
-                    this.currentOrder.vs_channel_id = channelId;
-                    
-                    this.vapeState.renderTrigger++;
+                    const id = selected.id ?? selected.item?.id;
+                    this.currentOrder.vs_channel_id = id;
+                    this.vapeUiState.channelId = id;
                 }
             },
         });
@@ -59,13 +77,14 @@ patch(PaymentScreen.prototype, {
     async openShippingPopup() {
         this.dialog.add(CustomGridPopup, {
             title: "Seleccionar Tipo de Envío",
-            list: this.pos.vape_shipping_types.map(s => ({ id: s.id, label: s.name, item: s })),
+            list: this.pos.vape_shipping_types.map(s => ({
+                id: s.id, label: s.name, item: s
+            })),
             getPayload: (selected) => {
                 if (selected) {
-                    const shippingId = selected.id || selected.item?.id || selected;
-                    this.currentOrder.vs_shipping_type_id = shippingId;
-                    
-                    this.vapeState.renderTrigger++;
+                    const id = selected.id ?? selected.item?.id;
+                    this.currentOrder.vs_shipping_type_id = id;
+                    this.vapeUiState.shippingTypeId = id;
                 }
             },
         });
